448410e0e030c41406dc229f60fd7043e2ea76f9
devjihad <mdhajihad609@gmail.com>
2026-01-20 12:48:31 +0600

    done

--- logic.js
+++ logic.js
@@ -1,10 +1,33 @@
-let increase =''
-
-for(let i=0; i<50; i++){
+const element=()=>{
+    let increase =''
+for(let i=0; i<150; i++){
     let num = Math.floor(Math.random()*10)
      increase += `<div class="bottomElement">${num}</div>`
-     console.log(num)
+    //  console.log(num)
+}
+document.querySelector('.bottom').innerHTML = increase
+}
+element()
+
+const timestamp=()=>{
+    let time =60 
+    let timer = setInterval(() => {
+        if(time >0){
+            time--
+            document.querySelector('#time').innerHTML = time
+        }
+        else{
+            clearInterval(timer)
+        }
+    }, 1000);
 }
 
+timestamp()
 
-document.querySelector('.bottom').innerHTML = increase
+const hitnum =()=>{
+    let hit = Math.floor(Math.random()*10)
+    document.querySelector('#hitt').textContent = hit
+    console.log(hit)
+}
+
+hitnum()
